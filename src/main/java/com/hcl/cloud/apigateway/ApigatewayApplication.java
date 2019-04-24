78243225104adb8ed2d8f1ddb35e44b506668794
MohitKri <MohitKri@LP-5CD9027VP6.HCLT.CORP.HCL.IN>
2019-04-24 16:46:44 +0530

    updated plugins

--- src/main/java/com/hcl/cloud/apigateway/ApigatewayApplication.java
+++ src/main/java/com/hcl/cloud/apigateway/ApigatewayApplication.java
@@ -5,13 +5,29 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
+/**
+ * @author MohitKri
+ *
+ */
 @EnableZuulProxy
 @EnableDiscoveryClient
 @SpringBootApplication
 public class ApigatewayApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ApigatewayApplication.class, args);
-	}
+    /**
+     * .
+     *
+     * @param args
+     *            the argument for main
+     */
+    public static void main(final String[] args) {
+        SpringApplication.run(ApigatewayApplication.class, args);
+    }
+
+    /**
+     *
+     */
+    public void pmd() {
+    }
 
 }
